9f256c53dd3f339fd315a8aa86b2579e346ea16c
Miguel <miguel.castillo-melendez81@bcmail.cuny.edu>
2023-08-14 14:12:38 -0400

    onclick different approach.

---
+++ DOM/Inline-Events/app.js
@@ -0,0 +1,13 @@
+const button = document.querySelector('#v2')
+
+button.onclick = function () {
+  console.log('You clicked me!')
+  console.log('I hope it works...')
+}
+
+function scream() {
+  console.log('Ahhhhhh')
+  console.log('Stop touching me')
+}
+
+button.onmouseenter = scream // passing the function, not executing. executed when button is pressed

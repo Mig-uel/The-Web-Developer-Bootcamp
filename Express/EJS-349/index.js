400518de8dc017474fd494f87d06c1d95383af46
Miguel <miguel.castillo-melendez81@bcmail.cuny.edu>
2023-09-21 17:29:25 -0400

    reorg.

--- Express/EJS-349/index.js
+++ Express/EJS-349/index.js
@@ -1,12 +1,12 @@
+// Express
+const express = require('express')
+const app = express()
+const path = require('path')
+
 // Routes
 const { defaultRoute } = require('./routes/getRoutes')
 
-const express = require('express')
-const app = express()
 const port = 3000
-
-const path = require('path')
-
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, '/views'))
 

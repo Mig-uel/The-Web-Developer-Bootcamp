9e98ea50cdf3210ef9e46f27fbc2edb21f7849fe
Miguel <miguel.castillo-melendez81@bcmail.cuny.edu>
2023-09-21 14:47:16 -0400

    routing: get, post, delete, put

--- Express/Express-App-341/app.js
+++ Express/Express-App-341/app.js
@@ -2,8 +2,36 @@ const express = require('express')
 const port = 3000
 const app = express()
 
-app.use((req, res) => {
+// middleware
+app.use((req, res, next) => {
   console.log('NEW REQUEST!')
+  console.log(`User requested: ${req.url}`)
+  next()
+})
+
+// /cats route (GET)
+app.get('/cats', (req, res) => {
+  res.send('<h1>Cats</h1>')
+})
+
+// /cats route (POST)
+app.post('/cats', (req, res) => {
+  res.send({ response: 'POSTED TO /cats' })
+})
+
+// /dogs route
+app.get('/dogs', (req, res) => {
+  res.send('<h1>Dogs</h1>')
+})
+
+// '/'
+app.get('/', (req, res) => {
+  res.send('<h1>Home</h1>')
+})
+
+// 404 Handler
+app.get('*', (req, res) => {
+  res.write('<h1>404 Page Not Found</h1>')
 })
 
 app.listen(port, () => {

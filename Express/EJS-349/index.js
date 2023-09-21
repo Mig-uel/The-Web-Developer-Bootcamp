7a66beb1c74525d118024ca67bf3f4c7715fd472
Miguel <miguel.castillo-melendez81@bcmail.cuny.edu>
2023-09-21 17:24:10 -0400

    path.

--- Express/EJS-349/index.js
+++ Express/EJS-349/index.js
@@ -5,7 +5,10 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const path = require('path')
+
 app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, '/views'))
 
 app.get('/', defaultRoute)
 

b5392c8bf7fab61a9f64b872b8a5838908a7d1c5
Miguel <miguel.castillo-melendez81@bcmail.cuny.edu>
2023-09-21 14:34:31 -0400

    setting up express, use, listen

---
+++ Express/Express-App-341/app.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const port = 3000
+const app = express()
+
+app.use(() => {
+  console.log('NEW REQUEST!')
+})
+
+app.listen(port, () => {
+  console.log('Listening on port 3000')
+})

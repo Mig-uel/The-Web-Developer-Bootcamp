eb7e8397b1cfcca554c4b327d43cfbf0c9f9fe8d
Miguel <miguel.castillo-melendez81@bcmail.cuny.edu>
2023-09-21 17:37:33 -0400

    passing data.

--- Express/EJS-349/index.js
+++ Express/EJS-349/index.js
@@ -4,13 +4,14 @@ const app = express()
 const path = require('path')
 
 // Routes
-const { defaultRoute } = require('./routes/getRoutes')
+const { defaultGetRoute, randomNumberGetRoute } = require('./routes/getRoutes')
 
 const port = 3000
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, '/views'))
 
-app.get('/', defaultRoute)
+app.get('/random', randomNumberGetRoute)
+app.get('/', defaultGetRoute)
 
 app.listen(port, () => {
   console.log('SERVER STARTED!')
--- Express/EJS-349/routes/getRoutes.js
+++ Express/EJS-349/routes/getRoutes.js
@@ -1,7 +1,14 @@
-const defaultRoute = (req, res) => {
+const defaultGetRoute = (req, res) => {
   res.render('home')
 }
 
+const randomNumberGetRoute = (req, res) => {
+  const randomNumber = Math.floor(Math.random() * 10) + 1
+
+  res.render('random', { randomNumber })
+}
+
 module.exports = {
-  defaultRoute,
+  defaultGetRoute,
+  randomNumberGetRoute,
 }

3b7cff662283c4a60b9f0ba3465bdd858cfbc24f
Miguel <miguel.castillo-melendez81@bcmail.cuny.edu>
2023-09-25 13:10:37 -0400

    parsing incoming req.

--- Restful/POST-362/index.js
+++ Restful/POST-362/index.js
@@ -3,12 +3,16 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
 app.get('/tacos', (req, res) => {
   res.send('GET /tacos response')
 })
 
 app.post('/tacos', (req, res) => {
-  res.send({ response: 'POST /tacos response' })
+  const { meat, qty } = req.body
+  res.send({ response: 'POST /tacos response', data: [meat, qty] })
 })
 
 app.listen(port, () => {

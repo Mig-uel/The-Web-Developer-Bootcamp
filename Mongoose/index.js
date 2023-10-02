389cea1942674e646c71e6a37968842105355873
Miguel <35431824+Mig-uel@users.noreply.github.com>
2023-10-02 13:28:23 -0400

    mongoose connection.

--- Mongoose/index.js
+++ Mongoose/index.js
@@ -1,8 +1,13 @@
-const express = require('express')
+// const express = require('express')
+const mongoose = require('mongoose')
 
-const port = 3000
-const app = express()
+// const port = 3000
+// const app = express()
+mongoose
+  .connect('mongodb://127.0.0.1:27017/movieDB')
+  .then(() => console.log('CONNECTION OPENED...'))
+  .catch((e) => console.log(e))
 
-app.listen(port, () => {
-  console.log(`SERVER STARTED ON PORT: ${port}`)
-})
+// app.listen(port, () => {
+//   console.log(`SERVER STARTED ON PORT: ${port}`)
+// })

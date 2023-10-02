af7acd115176f25259d07de77e668dc84cdedbf7
Miguel <35431824+Mig-uel@users.noreply.github.com>
2023-10-02 15:52:52 -0400

    more schema constraints.

--- Mongoose/product-398/index.js
+++ Mongoose/product-398/index.js
@@ -1,12 +1,16 @@
 const mongoose = require('mongoose')
-const { Product } = require('./models/product.model')
+const { Product } = require('./models/product.schema')
 
 mongoose
   .connect('mongodb://127.0.0.1:27017/shopApp')
   .then(() => console.log('CONNECTION STARTED...'))
   .catch((e) => console.log(e))
 
-const bike = new Product({ name: 'Mountain Bike', price: 599 })
+const bike = new Product({
+  name: 'Bike Helmet',
+  price: 19.5,
+  categories: ['Cycling', 'Safety'],
+})
 
 bike
   .save()
--- Mongoose/product-398/models/product.model.js
+++ Mongoose/product-398/models/product.model.js
@@ -2,8 +2,18 @@ const productModel = {
   name: {
     type: String,
     required: true,
+    maxLength: 20,
   },
-  price: { type: Number },
+  price: { type: Number, min: 0 },
+  onSale: {
+    type: Boolean,
+    default: false,
+  },
+  categories: [String],
+  qty: {
+    online: { type: Number, default: 0 },
+    inStore: { type: Number, default: 0 },
+  },
 }
 
 module.exports = productModel

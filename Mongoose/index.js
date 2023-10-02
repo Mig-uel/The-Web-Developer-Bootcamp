f4d8b71c1f3f297ce26d5f2b33d47336cd9bf4d3
Miguel <35431824+Mig-uel@users.noreply.github.com>
2023-10-02 13:51:58 -0400

    inserting into movieDB.

--- Mongoose/index.js
+++ Mongoose/index.js
@@ -1,13 +1,15 @@
-// const express = require('express')
 const mongoose = require('mongoose')
+const { Movie } = require('./models/movie.schema')
 
-// const port = 3000
-// const app = express()
 mongoose
   .connect('mongodb://127.0.0.1:27017/movieDB')
   .then(() => console.log('CONNECTION OPENED...'))
   .catch((e) => console.log(e))
 
-// app.listen(port, () => {
-//   console.log(`SERVER STARTED ON PORT: ${port}`)
-// })
+Movie.insertMany([
+  { title: 'Amelie', year: 2001, score: 8.3, rating: 'R' },
+  { title: 'Alien', year: 1979, score: 8.1, rating: 'R' },
+  { title: 'The Iron Giant', year: 1999, score: 7.5, rating: 'PG' },
+  { title: 'Stand By Me', year: 1986, score: 8.6, rating: 'R' },
+  { title: 'Moonrise Kingdom', year: 2012, score: 7.3, rating: 'PG-13' },
+]).then((data) => console.log(data))

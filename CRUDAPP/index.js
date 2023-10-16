eed67b1294af7c497fe4f29229c593907f139e1d
Miguel <35431824+Mig-uel@users.noreply.github.com>
2023-10-16 17:21:15 -0400

    add product route and template and post route.

--- CRUDAPP/index.js
+++ CRUDAPP/index.js
@@ -14,10 +14,22 @@ mongoose
 
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
+app.use(express.urlencoded({ extended: true }))
 
 app.get('/products', async (req, res) => {
   const products = await Product.find({})
   res.render('products/index', { products })
+})
+
+app.get('/products/add', (req, res) => {
+  res.render('products/add')
+})
+
+app.post('/products', async (req, res) => {
+  const newProduct = new Product(req.body)
+  await newProduct.save()
+
+  res.redirect(`/products/${newProduct._id}`)
 })
 
 app.get('/products/:id', async (req, res) => {

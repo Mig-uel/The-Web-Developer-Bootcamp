b3db6d6a060d87d914a75b844934783a0d940a5e
Miguel <35431824+Mig-uel@users.noreply.github.com>
2023-10-16 15:43:56 -0400

    express init.

---
+++ CRUDAPP/index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+const port = 3000
+
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+
+app.listen(port, () => {
+  console.log('SERVER STARTED')
+})

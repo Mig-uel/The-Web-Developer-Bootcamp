61e2dbea5b3b2d1aa04a6e7bcc7ca295ad171165
Miguel <35431824+Mig-uel@users.noreply.github.com>
2023-10-16 15:49:32 -0400

    setup mongodb connection.

--- CRUDAPP/index.js
+++ CRUDAPP/index.js
@@ -1,11 +1,21 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const path = require('path')
 
 const app = express()
 const port = 3000
 
+mongoose
+  .connect('mongodb://127.0.0.1:27017/crudApp')
+  .then(() => console.log('CONNECTED TO MONGO DATABASE'))
+  .catch((err) => console.log(err))
+
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
+
+app.get('/dogs', (req, res) => {
+  res.send('WOOF!')
+})
 
 app.listen(port, () => {
   console.log('SERVER STARTED')

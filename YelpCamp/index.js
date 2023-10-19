fb81f5aa68b37eee97eb88b263460d487c552b89
Miguel <35431824+Mig-uel@users.noreply.github.com>
2023-10-19 17:15:27 -0400

    created campground model and mongo connection.

--- YelpCamp/index.js
+++ YelpCamp/index.js
@@ -1,5 +1,15 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const path = require('path')
+
+// campground model
+const Campground = require('./model/campground.model')
+
+// mongoose local database
+mongoose
+  .connect('mongodb://127.0.0.1:27017/yelpcamp')
+  .then(() => console.log('DATABASE CONNECTED'))
+  .catch((e) => console.log(e))
 
 const app = express()
 const port = 3000

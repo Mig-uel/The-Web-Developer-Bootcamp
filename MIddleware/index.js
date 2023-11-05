4c6e26a174080d553be87b4bce743c85c090c0cc
Miguel <35431824+Mig-uel@users.noreply.github.com>
2023-11-04 21:15:34 -0400

    middleware with path/route.

--- MIddleware/index.js
+++ MIddleware/index.js
@@ -11,22 +11,34 @@ app.use(morgan('tiny'))
 //   next()
 // })
 
-app.use((req, res, next) => {
-  req.requestTime = Date.now()
+// app.use((req, res, next) => {
+//   req.requestTime = Date.now()
 
-  console.log(req.method, req.path)
+//   console.log(req.method, req.path)
+//   return next()
+// })
+
+// Middleware w/ Path
+app.use('/dogs', (req, res, next) => {
+  console.log('I LOVE DOGS <3')
+
   return next()
 })
 
 //Routes
 app.get('/', (req, res) => {
-  console.log(req.requestTime)
+  // console.log(req.requestTime)
 
   res.send('Homepage!')
 })
 
 app.get('/dogs', (req, res) => {
   res.send('WOOF WOOF!')
+})
+
+// 404 Page
+app.use((req, res) => {
+  res.send('404 NOT FOUND...')
 })
 
 app.listen(port, () => console.log(`SERVER STARTED ON PORT ${port}`))

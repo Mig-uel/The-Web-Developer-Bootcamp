7aacf5eadcc70842a9dc764feca5f3a32a8e9af2
Miguel <35431824+Mig-uel@users.noreply.github.com>
2023-11-07 14:44:32 -0500

    error handling class..

--- MIddleware/index.js
+++ MIddleware/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const morgan = require('morgan')
+const AppError = require('./AppError')
 
 const app = express()
 const port = 3000
@@ -33,7 +34,8 @@ const verifyPassword = (req, res, next) => {
     return next()
   }
 
-  res.send('SORRY YOU NEED A PASSWORD!')
+  throw new AppError('Password required!', 401)
+  // res.send('SORRY YOU NEED A PASSWORD!')
 }
 
 //Routes
@@ -52,9 +54,32 @@ app.get('/secret', verifyPassword, (req, res) => {
   res.send('YOU ARE IN!')
 })
 
+app.get('/error', (req, res) => {
+  chicken.fly()
+})
+
+app.get('/admin', (req, res) => {
+  throw new AppError('You do not have Admin privileges!', 403)
+})
+
 // 404 page
 app.use((req, res) => {
   res.send('404 NOT FOUND...')
+})
+
+// Error Handling
+// app.use((err, req, res, next) => {
+//   console.log('**************************')
+//   console.log('********ERROR**********')
+//   console.log('**************************')
+//   console.log(err)
+
+//   next(err)
+// })
+
+app.use((err, req, res, next) => {
+  const { status = 500, message = 'Something went wrong!' } = err
+  res.status(status).send(message)
 })
 
 app.listen(port, () => console.log(`SERVER STARTED ON PORT ${port}`))

399b9a481c8db8c0bdc2fbeee9afe4fc3d88d4fa
Miguel <35431824+Mig-uel@users.noreply.github.com>
2023-11-14 16:06:25 -0500

    joi schema file.

--- YelpCamp/index.js
+++ YelpCamp/index.js
@@ -5,7 +5,7 @@ const methodOverride = require('method-override')
 const ejsMate = require('ejs-mate')
 const catchAsync = require('./utils/catchAsync')
 const ExpressError = require('./utils/ExpressError')
-const Joi = require('joi')
+const campgroundSchema = require('./schemas')
 
 // campground model
 const Campground = require('./model/campground.model')
@@ -24,6 +24,15 @@ app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+
+const validateCampground = (req, res, next) => {
+  const { error } = campgroundSchema.validate(req.body)
+
+  if (error) {
+    const msg = error.details.map((e) => e.message).join(',')
+    throw new ExpressError(msg, 400)
+  } else next()
+}
 
 app.get('/', (req, res) => {
   res.render('home')
@@ -44,27 +53,8 @@ app.get('/campgrounds/add', (req, res) => {
 
 app.post(
   '/campgrounds',
+  validateCampground,
   catchAsync(async (req, res) => {
-    // if (!req.body.campground)
-    //   throw new ExpressError('Invalid Campground Data!', 400)
-
-    const campgroundSchema = Joi.object({
-      campground: Joi.object({
-        title: Joi.string().required(),
-        price: Joi.number().required().min(0),
-        image: Joi.string().required(),
-        location: Joi.string().required(),
-        description: Joi.string().required(),
-      }).required(),
-    })
-
-    const { error } = campgroundSchema.validate(req.body)
-
-    if (error) {
-      const msg = error.details.map((e) => e.message).join(',')
-      throw new ExpressError(msg, 400)
-    }
-
     const campground = new Campground(req.body.campground)
     await campground.save()
 
@@ -95,6 +85,7 @@ app.get(
 
 app.put(
   '/campgrounds/:id',
+  validateCampground,
   catchAsync(async (req, res) => {
     const { id } = req.params
 

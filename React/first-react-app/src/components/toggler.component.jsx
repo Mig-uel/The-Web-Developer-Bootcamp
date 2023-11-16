a091b729aaad7fb49b162bb97c2c4c88074dc6f4
Miguel <35431824+Mig-uel@users.noreply.github.com>
2023-11-16 14:52:21 -0500

    multiple states.

--- React/first-react-app/src/components/toggler.component.jsx
+++ React/first-react-app/src/components/toggler.component.jsx
@@ -2,13 +2,18 @@ import { useState } from 'react'
 
 const Toggler = () => {
   const [isHappy, setIsHappy] = useState(true)
+  const [count, setCount] = useState(0)
 
   const toggleIsHappy = () => setIsHappy(!isHappy)
 
   return (
-    <h1 onClick={toggleIsHappy} style={{ cursor: 'pointer' }}>
-      {isHappy ? '😊' : '😭'}
-    </h1>
+    <>
+      <h1 onClick={toggleIsHappy} style={{ cursor: 'pointer' }}>
+        {isHappy ? '😊' : '😭'}
+      </h1>
+      <h1>Counter: {count}</h1>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+    </>
   )
 }
 

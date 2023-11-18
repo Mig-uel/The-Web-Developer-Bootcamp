a622d44ccda7a149df508a6b393b3433d8acb797
Miguel <35431824+Mig-uel@users.noreply.github.com>
2023-11-18 18:27:34 -0500

    accessing previous version of state.

--- React/state-demo/src/components/counter.component.jsx
+++ React/state-demo/src/components/counter.component.jsx
@@ -3,11 +3,17 @@ import { useState } from 'react'
 const Counter = () => {
   const [count, setCount] = useState(0)
 
-  const incrementCount = () => setCount(count + 1)
+  const addOne = () => setCount(count + 1)
+  const addThree = () => {
+    // setCount((c) => c + 1) // callback returns current value
+    setCount(count + 3)
+  }
+
   return (
     <div>
       <p>Count: {count}</p>
-      <button onClick={incrementCount}>+1</button>
+      <button onClick={addOne}>+1</button>
+      <button onClick={addThree}>+3</button>
     </div>
   )
 }

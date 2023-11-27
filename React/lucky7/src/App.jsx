859208ab1bb8fdc76778109de00cb08d5b5d37aa
Miguel <35431824+Mig-uel@users.noreply.github.com>
2023-11-27 14:47:49 -0500

    passing functions as props.

--- React/lucky7/src/App.jsx
+++ React/lucky7/src/App.jsx
@@ -1,9 +1,14 @@
 import LuckyN from './components/luckyN.component'
+import { sum } from './utils/utils'
+
+const lessThan4 = (dice) => sum(dice) < 4
+const allSameValues = (dice) => dice.every((v) => v === dice[0])
 
 function App() {
   return (
     <>
-      <LuckyN numDice={3} />
+      <LuckyN winCheck={lessThan4} />
+      <LuckyN winCheck={allSameValues} />
     </>
   )
 }
--- React/lucky7/src/components/luckyN.component.jsx
+++ React/lucky7/src/components/luckyN.component.jsx
@@ -1,17 +1,17 @@
 import { useState } from 'react'
-import { getRolls, sum } from '../utils/utils'
+import { getRolls } from '../utils/utils'
 import Dice from './dice.component'
 
-const LuckyN = ({ numDice = 2, goal = 7 }) => {
+const LuckyN = ({ numDice = 2, winCheck }) => {
   const [dice, setDice] = useState(getRolls(numDice))
-  const isWinner = sum(dice) === goal // boolean
+  const isWinner = winCheck(dice) // boolean
 
   const roll = () => setDice(getRolls(numDice))
 
   return (
     <main className='LuckyN'>
       <h1>
-        Lucky{goal} {isWinner && 'You Win!'}
+        Lucky {isWinner && 'You Win!'}
       </h1>
       <Dice dice={dice} />
       <button onClick={roll}>Re-Roll Dice</button>

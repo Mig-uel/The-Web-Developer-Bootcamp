e8708550f8db9ab64f0d7fdaeca91d3bcbd53591
Miguel <35431824+Mig-uel@users.noreply.github.com>
2023-11-30 11:14:49 -0500

    added second state, useEffect can accept second parameter, [ ] of dependencies of when to run useEffect func.

--- React/react-effects/src/components/counter.component.jsx
+++ React/react-effects/src/components/counter.component.jsx
@@ -3,12 +3,22 @@ import { useState, useEffect } from 'react'
 const Counter = () => {
   const [count, setCount] = useState(0)
 
+  const [name, setName] = useState('')
+
+  useEffect(() => {
+    console.log('EFFECT CALLED')
+  }, [])
+
   const increment = () => setCount((oldCount) => oldCount + 1)
+  const handleChange = (e) => setName(e.target.value)
 
   return (
     <div>
       <h1>{count}</h1>
       <button onClick={increment}>+1</button>
+
+      <p>{name}</p>
+      <input type='text' value={name} onChange={handleChange} />
     </div>
   )
 }

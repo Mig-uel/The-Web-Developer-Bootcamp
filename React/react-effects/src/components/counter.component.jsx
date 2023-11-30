2e7a8aca65f113eb0609fbfa15fe70750ddb2e26
Miguel <35431824+Mig-uel@users.noreply.github.com>
2023-11-30 10:23:48 -0500

    counter component created.

---
+++ React/react-effects/src/components/counter.component.jsx
@@ -0,0 +1,16 @@
+import { useState, useEffect } from 'react'
+
+const Counter = () => {
+  const [count, setCount] = useState(0)
+
+  const increment = () => setCount((oldCount) => setCount(oldCount + 1))
+
+  return (
+    <div>
+      <h1>{count}</h1>
+      <button onClick={increment}>+1</button>
+    </div>
+  )
+}
+
+export default Counter

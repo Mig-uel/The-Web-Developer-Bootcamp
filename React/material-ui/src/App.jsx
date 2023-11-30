f7302622ac2501a6559674580a2c6a23c672f5b7
Miguel <35431824+Mig-uel@users.noreply.github.com>
2023-11-30 12:15:46 -0500

    imported alarm iconn.

--- React/material-ui/src/App.jsx
+++ React/material-ui/src/App.jsx
@@ -1,11 +1,16 @@
+import { Button, IconButton } from '@mui/material/'
 import './App.css'
 
-import Button from '@mui/material/Button'
+import AlarmIcon from '@mui/icons-material/Alarm'
 
 function App() {
   return (
     <div>
       <Button variant='contained'>Contained</Button>
+
+      <IconButton color='secondary' aria-label='add an alarm'>
+        <AlarmIcon />
+      </IconButton>
     </div>
   )
 }

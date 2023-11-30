a5ec9e29e6051106647652fdf89a3523d6259fae
Miguel <35431824+Mig-uel@users.noreply.github.com>
2023-11-30 14:37:21 -0500

    renamed file.

---
+++ React/mui-todo/src/components/todo-list.component.jsx
@@ -0,0 +1,51 @@
+import { List } from '@mui/material'
+
+import TodoItem from './todo-item.component'
+import TodoForm from './todo-form.component'
+
+import { useState } from 'react'
+import { v4 as uuid } from 'uuid'
+
+const initialTodos = [
+  { id: uuid(), text: 'Walk the dog', completed: false },
+  { id: uuid(), text: 'Walk the cat', completed: false },
+  { id: uuid(), text: 'Feed the fish', completed: false },
+  { id: uuid(), text: 'Feed the chickens', completed: false },
+]
+
+const TodoList = () => {
+  const [todos, setTodos] = useState(initialTodos)
+
+  const removeTodo = (id) =>
+    setTodos((prevTodos) => prevTodos.filter((todo) => todo.id !== id))
+
+  const toggleTodo = (id) =>
+    setTodos((prevTodos) => {
+      return prevTodos.map((todo) => {
+        if (todo.id === id) return { ...todo, completed: !todo.completed }
+        else return todo
+      })
+    })
+
+  const addTodo = (text) =>
+    setTodos((prevTodos) => [
+      ...prevTodos,
+      { text: text, id: uuid(), completed: false },
+    ])
+
+  return (
+    <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+      {todos.map((todo) => (
+        <TodoItem
+          key={todo.id}
+          todo={todo}
+          removeTodo={() => removeTodo(todo.id)}
+          toggleTodo={() => toggleTodo(todo.id)}
+        />
+      ))}
+      <TodoForm addTodo={addTodo} />
+    </List>
+  )
+}
+
+export default TodoList
